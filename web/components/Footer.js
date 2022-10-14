80510f861e293cd989ff3f2a2178e09fec72bd74
pelumioy <pelumioy@gmail.com>
2022-10-14 19:16:13 +0100

    ...

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <>
     <hr />
-    <div className="text-center">
-        <h1>Made with ❤️ by <a href="">Pelzzyyy</a></h1>
+    <div className="text-center p-3 text-blue-500 font-semibold">
+        <h1>Made with ❤️ by <a href="https://github.com/pelumioy">Pelzzyyy</a></h1>
     </div>
     </>
   );
--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import AuthProvider from "../contexts/AuthContext";
 import "../styles/globals.css";
 
@@ -5,6 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
+      <Footer />
     </AuthProvider>
   );
 }
